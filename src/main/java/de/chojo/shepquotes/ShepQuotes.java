397709a5fe51fbb317afd82ceeece1022dafbd76
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-07-05 13:01:06 +0200

    Add more logging

--- src/main/java/de/chojo/shepquotes/ShepQuotes.java
+++ src/main/java/de/chojo/shepquotes/ShepQuotes.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import de.chojo.jdautil.command.dispatching.CommandHub;
 import de.chojo.jdautil.localization.Localizer;
 import de.chojo.jdautil.localization.util.Language;
+import de.chojo.jdautil.threading.ThreadFactories;
 import de.chojo.logutil.marker.LogNotify;
 import de.chojo.shepquotes.commands.Add;
 import de.chojo.shepquotes.commands.Edit;
@@ -23,6 +24,7 @@ import de.chojo.sqlutil.updater.QueryReplacement;
 import de.chojo.sqlutil.updater.SqlUpdater;
 import de.chojo.sqlutil.wrapper.QueryBuilderConfig;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.RestAction;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
@@ -31,25 +33,31 @@ import org.slf4j.Logger;
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class ShepQuotes {
     private static final Logger log = getLogger(ShepQuotes.class);
-    private static ShepQuotes instance;
+    private static ShepQuotes instance = null;
     private Configuration configuration;
+
+    private final ThreadGroup worker = new ThreadGroup("ShepQuotesWorker");
+    private ExecutorService executor;
     private HikariDataSource dataSource;
     private ShardManager shardManager;
     private QuoteData quoteData;
 
     public static void main(String[] args) throws SQLException, IOException {
-        instance = new ShepQuotes();
+        ShepQuotes.instance = new ShepQuotes();
         instance.start();
     }
 
     private void start() throws SQLException, IOException {
         configuration = Configuration.create();
+
+        RestAction.setDefaultFailure(err -> log.error(LogNotify.NOTIFY_ADMIN, "Unexpected error in async callback.", err));
 
         initDb();
 
@@ -90,37 +98,38 @@ public class ShepQuotes {
     private void initDb() throws IOException, SQLException {
         var dbLogger = getLogger("DbLogger");
         QueryBuilderConfig.setDefault(QueryBuilderConfig.builder()
-                .withExceptionHandler(err -> dbLogger.error("An error occured: {}", ExceptionTransformer.prettyException(err)))
+                .withExceptionHandler(err -> dbLogger.error(LogNotify.NOTIFY_ADMIN,"An error occured: {}", ExceptionTransformer.prettyException(err)))
                 .build());
 
-        var db = configuration.database();
+        var dbc = configuration.database();
         dataSource = DataSourceCreator.create(SqlType.POSTGRES)
                 .configure(builder -> builder
-                        .host(db.host())
-                        .port(db.port())
-                        .database(db.database())
-                        .user(db.user())
-                        .password(db.password()))
+                        .host(dbc.host())
+                        .port(dbc.port())
+                        .database(dbc.database())
+                        .user(dbc.user())
+                        .password(dbc.password()))
                 .create()
                 .build();
 
+
         SqlUpdater.builder(dataSource, SqlType.POSTGRES)
                 .withLogger(LoggerAdapter.wrap(dbLogger))
-                .setSchemas(db.schema())
-                .setReplacements(new QueryReplacement("shep_quotes", db.schema()))
+                .setSchemas(dbc.schema())
+                .setReplacements(new QueryReplacement("shep_quotes", dbc.schema()))
                 .execute();
 
         dataSource.close();
 
         dataSource = DataSourceCreator.create(SqlType.POSTGRES)
                 .configure(builder -> builder
-                        .host(db.host())
-                        .port(db.port())
-                        .database(db.database())
-                        .user(db.user())
-                        .password(db.password()))
+                        .host(dbc.host())
+                        .port(dbc.port())
+                        .database(dbc.database())
+                        .user(dbc.user())
+                        .password(dbc.password()))
                 .create()
-                .forSchema(db.schema())
+                .forSchema(dbc.schema())
                 .withMaximumPoolSize(5)
                 .build();
 
@@ -129,14 +138,23 @@ public class ShepQuotes {
     }
 
     private void initShardManager() throws LoginException {
+
+        var threadLogger = getLogger("ThreadLogger");
+        executor = Executors.newCachedThreadPool(ThreadFactories.builder()
+                .exceptionHandler((t, e) -> threadLogger.error(LogNotify.NOTIFY_ADMIN, ThreadFactories.threadErrorMessage(t), e))
+                .group(worker)
+                .build());
+
         shardManager = DefaultShardManagerBuilder.createLight(configuration.baseSettings().token())
                 .enableIntents(GatewayIntent.GUILD_MESSAGES)
                 .enableCache(CacheFlag.MEMBER_OVERRIDES)
-                .setEventPool(Executors.newCachedThreadPool())
+                .setEventPool(executor)
                 .build();
     }
 
     public void shutdown() {
-
+        shardManager.shutdown();
+        dataSource.close();
+        executor.shutdown();
     }
 }
